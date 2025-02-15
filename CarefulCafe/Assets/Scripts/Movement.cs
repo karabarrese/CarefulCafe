c0f4a2a0688544660955bbfd0a807156fb5deb07
TiffanyN22 <tnguyen22@ndsj.org>
2025-02-15 11:49:12 -0800

    Animate character movement

---
+++ CarefulCafe/Assets/Scripts/Movement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+    public Animator animator;
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, vertical);
+        AnimateMovement(direction);
+        transform.position += direction * speed * Time.deltaTime;
+    }
+
+    void AnimateMovement(Vector3 direction){
+        if(animator == null){
+            return;
+        }
+        if(direction.magnitude > 0) {
+            animator.SetBool("isMoving", true);
+
+            animator.SetFloat("horizontal", direction.x);
+            animator.SetFloat("vertical", direction.y);
+        } else {
+            animator.SetBool("isMoving", false);
+        }
+    }
+}
