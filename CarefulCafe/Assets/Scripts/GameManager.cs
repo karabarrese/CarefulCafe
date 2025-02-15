59475e8e8c12f4a84082f3bc6498a233fe857185
TiffanyN22 <tnguyen22@ndsj.org>
2025-02-15 12:07:39 -0800

    Basic game state machine

--- CarefulCafe/Assets/Scripts/GameManager.cs
+++ CarefulCafe/Assets/Scripts/GameManager.cs
@@ -1,18 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    // Game manager variables
+    public static GameManager Instance { get; private set; }
+    public int score = 0; // TODO: customers served? money made?
+
+    // Keep track of step
+
+    public enum Step {ORDER, PANTRY_MINIGAME, BAKING_MINIGAME, WASHING_MINIGAME, GIVE_ORDER}
+    private Step curStep;
+    private Step prevStep;
+
+    // Variables for scene
+    [SerializeField] private GameObject player;
+
+    private void Start(){
+        curStep = Step.ORDER;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        // Ensure only one instance of GameManager exists
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); // Persist between scenes
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Update(){
+        switch (curStep){
+            case Step.ORDER:
+                break;
+            case Step.PANTRY_MINIGAME:
+                break;
+            case Step.BAKING_MINIGAME:
+                break;
+            case Step.WASHING_MINIGAME:
+                break;
+            case Step.GIVE_ORDER:
+                break;
+        }
     }
 }
