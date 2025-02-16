9da7f6cfadc10e616a28c00c4d802144886a8b40
rose413 <irenechang949@gmail.com>
2025-02-15 16:18:05 -0800

    Changed the ArrowScript and BarScript files

--- CarefulCafe/Assets/Scripts/Epipen/ArrowScript.cs
+++ CarefulCafe/Assets/Scripts/Epipen/ArrowScript.cs
@@ -4,35 +4,72 @@ using UnityEngine;
 
 public class ArrowScript : MonoBehaviour
 {
-    public Rigidbody2D arrowrigidbody;
-    public bool arrowisMoving = true;
-    // Start is called before the first frame update
+    public BarScript bar; // Reference to the Bar object
+    public Rigidbody2D rigid; // Reference to the Rigidbody2D component of the arrow
+    public float speed; // Speed of the arrow's movement
+    private bool movingUp = true; // Whether the arrow is moving up or down
+    private float currentY; // Current Y position of the arrow
+
+    private bool arrowisMoving = true; // Flag to control whether the arrow is moving
+
     void Start()
     {
-
+        bar = GameObject.FindGameObjectWithTag("Bar").GetComponent<BarScript>(); // Find the Bar object and get its script
+        currentY = transform.position.y + 2; // Get the initial Y position of the arrow
+        Debug.Log("Current Y position of the arrow is: " + currentY);        
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // while bool is true, the arrow will move up and down to a certain height and will stop once user clicks on space bar
-        if (arrowrigidbody.position.y > -3.5)
+        // If spacebar is pressed, stop the arrow
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            Debug.Log("Arrow has reached the bottom");
-            arrowrigidbody.velocity = Vector2.up * 2;
-        }
-        else if (arrowrigidbody.position.y <= 3.5)
-        {
-            Debug.Log("Arrow has reached the top");
-            arrowrigidbody.velocity = Vector2.down * 2;
+            arrowisMoving = false; // Toggle the movement on or off
+            rigid.velocity = Vector2.zero; // Stop the arrow's movement
+
         }
 
-        if (Input.GetKeyDown(KeyCode.Space) == true)
+        if (arrowisMoving)
         {
-            arrowisMoving = false;
-            arrowrigidbody.velocity = Vector2.zero;
+
+            MoveArrow();
         }
-
-
     }
+
+    void MoveArrow()
+    {
+        Debug.Log("Arrow is moving :()");
+        Debug.Log("Height of the bar is: " + bar.GetHeight());
+        // Get the height of the bar
+        float barHeight = bar.GetHeight();
+
+        // Check the current direction of the arrow and move it
+        if (movingUp)
+        {
+            // If the arrow has reached the top, switch direction
+            if (transform.position.y >= currentY + barHeight + 1)
+            {
+                movingUp = false;
+            }
+            else
+            {
+                // Move the arrow up
+                transform.Translate(Vector2.up * speed * Time.deltaTime);
+            }
+        }
+        else
+        {
+            // If the arrow has reached the bottom, switch direction
+            if (transform.position.y <= currentY - barHeight)
+            {
+                movingUp = true;
+            }
+            else
+            {
+                // Move the arrow down
+                transform.Translate(Vector2.down * speed * Time.deltaTime);
+            }
+        }
+    }
+
 }
--- CarefulCafe/Assets/Scripts/Epipen/BarScript.cs
+++ CarefulCafe/Assets/Scripts/Epipen/BarScript.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class BarScript : MonoBehaviour
 {
-    public float height = 10f;
+    public float height;
     // Start is called before the first frame update
     public float GetHeight()
     {
+        height = transform.localScale.y;
+        Debug.Log("Height of the bar is: " + height);
         return height;
     }
 }
