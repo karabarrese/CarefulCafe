3f41a225ac1a548da351f1ef769d06650cca6717
TiffanyN22 <tnguyen22@ndsj.org>
2025-02-15 21:58:28 -0800

    Updated kitchen background image and added collision to walls

--- CarefulCafe/Assets/Scripts/Movement.cs
+++ CarefulCafe/Assets/Scripts/Movement.cs
@@ -7,6 +7,7 @@ public class Movement : MonoBehaviour
     public static Movement Instance;
     public float speed;
     public Animator animator;
+    private Vector3 direction;
 
     private void Awake()
     {
@@ -27,22 +28,33 @@ public class Movement : MonoBehaviour
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
 
-        Vector3 direction = new Vector3(horizontal, vertical);
+        direction = new Vector3(horizontal, vertical);
+
         AnimateMovement(direction);
+    }
+
+    private void FixedUpdate()
+    {
+        //move the player
+        direction.Normalize();
         transform.position += direction * speed * Time.deltaTime;
     }
 
-    void AnimateMovement(Vector3 direction){
-        if(animator == null){
-            return;
-        }
-        if(direction.magnitude > 0) {
-            animator.SetBool("isMoving", true);
-
-            animator.SetFloat("horizontal", direction.x);
-            animator.SetFloat("vertical", direction.y);
-        } else {
-            animator.SetBool("isMoving", false);
+    void AnimateMovement(Vector3 direction)
+    {
+        direction.Normalize(); //so diagonal isn't faster
+        if (animator != null)
+        {
+            if(direction.magnitude > 0) //check if moving
+            {
+                animator.SetBool("isMoving", true);
+                animator.SetFloat("horizontal", direction.x);
+                animator.SetFloat("vertical", direction.y);
+            }
+            else
+            {
+                animator.SetBool("isMoving", false);
+            }
         }
     }
 }
