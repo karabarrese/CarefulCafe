7c3f6fc63dad67b9bec49d2dfeb3d1a9b2c755b7
Kara Barrese <karabarrese@gmail.com>
2025-02-15 18:06:11 -0800

    egg work

--- CarefulCafe/Assets/bowl.cs
+++ CarefulCafe/Assets/bowl.cs
@@ -4,9 +4,39 @@ using UnityEngine;
 
 public class bowl : MonoBehaviour
 {
+    public GameObject egg;
+
+    public GameObject crackedEgg;
+    public GameObject rawEgg;
+    private bool eggActive = true;
+
     private void OnTriggerStay2D(Collider2D other)
     {
         Debug.Log("Egg is in the bowl!");
+        eggActive = false;
+
+    }
+    void Start()
+    {
+        crackedEgg.SetActive(false);
+        rawEgg.SetActive(false);
     }
 
+    void Update() {
+        if (!eggActive)
+        {
+            StartCoroutine(WaitForEgg());
+        }
+    }
+    IEnumerator WaitForEgg()
+    {
+        eggActive = true;
+        yield return new WaitForSeconds(1);
+        egg.SetActive(false);
+        Debug.Log("Egg gone");
+        crackedEgg.SetActive(true);
+        yield return new WaitForSeconds(1);
+        crackedEgg.SetActive(false);
+        rawEgg.SetActive(true);
+    }
 }
